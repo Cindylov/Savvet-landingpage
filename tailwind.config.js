e599659eaa95189a08f770f6aa39a74e43a20c48
Cindylov <cynthiaujunwa4@gmail.com>
2024-12-19 15:58:58 +0100

    savvet

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary1:'#3B3548',
+        primary2: '#F76C5E',
+        hover1: '#B892FF',
+      },
+
+    },
+  },
+  plugins: [],
+}
+
